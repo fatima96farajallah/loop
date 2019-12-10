4f245b20d28c80bcb9747c876e4eb5ec3940a0ca
kalel96awwad <167565@ppu.edu.ps>
2019-12-10 19:06:10 +0200

    first

---
+++ loop.js
@@ -0,0 +1,9 @@
+
+function forLoop(exp, element) {
+    let arr = exp.split('of')[1];
+    let array = eval(arr);
+    let i = arr;
+    for(arr.length,i=0; i<arr.length;i++){
+        element.parent.innerHTML += element;
+    }
+}
