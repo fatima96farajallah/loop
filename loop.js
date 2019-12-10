ee1bdd072ca46c4c052c523a7531820656d03434
kalel96awwad <167565@ppu.edu.ps>
2019-12-10 20:15:25 +0200

    edit

--- loop.js
+++ loop.js
@@ -1,9 +1,15 @@
 
 function forLoop(exp, element) {
+
     let arr = exp.split('of')[1];
+    arr[1] = arr[1].replace("of");
     let array = eval(arr);
     let i = arr;
     for(arr.length,i=0; i<arr.length;i++){
         element.parent.innerHTML += element;
-    }
+    }  
 }
+let obj = {
+    exp: "n of arr",
+    element: (forLoop)
+  }
